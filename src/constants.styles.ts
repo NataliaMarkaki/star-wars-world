0e1669ff27b7adc77727e185e94ed7e692a8b516
Natalia Markaki <markaki.natalia@gmail.com>
2022-12-16 22:09:16 +0000

    feat: add styled components and common style constants

---
+++ src/constants.styles.ts
@@ -0,0 +1,14 @@
+// common colours
+
+export const colours = {
+  black: "#2F2F2F",
+  white: "#FFFFFF",
+  green: "#57BD84",
+  gray: "#F0F0F0",
+  red: "#8B0000",
+  transparent: "transparent",
+};
+
+export const media = {
+  medium: "768px",
+};
